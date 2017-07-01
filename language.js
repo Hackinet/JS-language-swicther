0a79e7e77115909e4cdbdd5cec59430f95de1423
Piyush Jha <Hackinet@users.noreply.github.com>
2017-07-01 10:25:49 +0530

    Add files via upload

---
+++ language.js
@@ -0,0 +1,45 @@
+
+
+function lang() {
+replaceTextOnPage('one', 'uno');
+replaceTextOnPage('two', 'dos');	
+replaceTextOnPage('three', 'tres');	
+replaceTextOnPage('four', 'las cuatro');	 
+replaceTextOnPage('five', 'cinco');	
+replaceTextOnPage('six', 'seis');
+replaceTextOnPage('seven', 'siete');
+replaceTextOnPage('eight', 'ocho');
+replaceTextOnPage('nine', 'nueve');
+replaceTextOnPage('ten', 'Ddiez');
+}; 
+          
+
+
+
+
+//CALLING THE FUNCTION
+
+
+function replaceTextOnPage(from, to){
+  getAllTextNodes().forEach(function(node){
+    node.nodeValue = node.nodeValue.replace(new RegExp(quote(from), 'g'), to);
+  });
+
+  function getAllTextNodes(){
+    var result = [];
+
+    (function scanSubTree(node){
+      if(node.childNodes.length) 
+        for(var i = 0; i < node.childNodes.length; i++) 
+          scanSubTree(node.childNodes[i]);
+      else if(node.nodeType == Node.TEXT_NODE) 
+        result.push(node);
+    })(document);
+
+    return result;
+  }
+
+  function quote(str){
+    return (str+'').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+  }
+}
